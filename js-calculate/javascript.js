10823ef41ca95b564604a0864c8e5eaae72209ce
Amin-4A6A <510074@edu.rocmn.nl>
2017-06-11 13:32:46 +0200

    ben er bijna

--- js-calculate/javascript.js
+++ js-calculate/javascript.js
@@ -1,16 +1,38 @@
 function uitRekenen() {
   var nummer = document.getElementById('numbers').value;
   var split = nummer.split(",");
+  var operator = document.getElementById('operation').value;
   var result = "";
 
   for (i = 0; i < split.length; i++) {
     split[i] = parseFloat(split[i])
   }
-    var een = split[0] + split[1];
-    console.log(een)
+
 
   document.getElementById('result').innerHTML = result;
 }
+// function reken() {
+//   result = split[0] + split[1];
+//   console.log(een)
+// }
+
+result = split[0];
+    for (var i=1; i < split.length; i++) {
+        result = reken(result, split[i], operator);
+    }
+
+function reken(operator,nbrOne,nbrTwo) {
+      if (operator == '+') {
+          result = nbrOne + nbrTwo;
+      } else if (operator == '-') {
+          result = nbrOne - nbrTwo;
+      } else if (operator == '*') {
+          result = nbrOne * nbrTwo;
+      } else if (operator == '/') {
+          result = nbrOne / nbrTwo;
+      }
+      return result;
+  }
   // console.log(split[i])
 
 
