410db820ee7657e564e789cb140fd4fd8eefeb66
Amin-4A6A <510074@edu.rocmn.nl>
2017-06-15 11:34:40 +0200

    hij count woorden

--- countwords/javascript.js
+++ countwords/javascript.js
@@ -1,5 +1,12 @@
 function myFunction() {
     var str = document.getElementById('text').value;
     var n = str.length;
-    document.getElementById("demo").innerHTML = n;
+    document.getElementById("letter").innerHTML = n;
+
+    var string = document.getElementById('text').value;
+    var split = string.split(" ");
+    var result = split.length;
+
+    console.log('result')
+    document.getElementById("words").innerHTML = result;
 }
